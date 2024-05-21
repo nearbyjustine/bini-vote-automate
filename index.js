83f07551622766f895f3389fa45454fa2d6d2813
Justine Castaneda <45316827+nearbyjustine@users.noreply.github.com>
2024-05-21 15:44:39 +0800

    random sleep timer to avoid bots

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { Builder, By, until } = require("selenium-webdriver");
 
 const assert = require("assert");
 
+function randomSleep() {
+  return Math.floor(Math.random() * 1000 + 2000);
+}
+
 async function voteBini(times) {
   let driver = await new Builder().forBrowser("chrome").build();
   driver.manage().window().maximize();
@@ -9,28 +13,28 @@ async function voteBini(times) {
   try {
     await driver.get("https://nylonmanila.com/big-bold-brave-awards-2024-favorite-p-pop-group/");
 
-    await driver.sleep(3000);
+    await driver.sleep(randomSleep());
 
     await driver.findElement(By.className("cky-btn-accept")).click();
 
-    await driver.sleep(3000);
+    await driver.sleep(randomSleep());
 
     for (let i = 0; i < times; i++) {
       // scroll to element
       console.log("step 1");
-      await driver.executeScript("window.scrollTo(0, 5000)");
+      await driver.executeScript("window.scrollTo(0, 5100)");
 
-      await driver.sleep(3000);
+      await driver.sleep(randomSleep());
 
       console.log("step 2");
       await driver.findElement(By.id("PDI_answer61480085")).click();
 
-      await driver.sleep(3000);
+      await driver.sleep(randomSleep());
 
       console.log("step 3");
       await driver.findElement(By.id("pd-vote-button13777388")).click();
 
-      await driver.sleep(3000);
+      await driver.sleep(randomSleep());
 
       console.log("step 3.1");
       const toSolve = await driver.findElement(By.xpath("//div[@id='captcha_13777388']/span/p")).getText();
@@ -43,12 +47,12 @@ async function voteBini(times) {
         console.log("step 4");
         await driver.findElement(By.id("answer_13777388")).sendKeys(result);
 
-        await driver.sleep(3000);
+        await driver.sleep(randomSleep());
 
         console.log("step 5");
         await driver.findElement(By.id("pd-vote-button13777388")).click();
 
-        await driver.sleep(3000);
+        await driver.sleep(randomSleep());
 
         // go back
 
@@ -62,6 +66,7 @@ async function voteBini(times) {
     console.error(e);
   } finally {
     await driver.quit();
+    voteBini(100);
   }
 }
 
